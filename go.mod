716882cf1e275e7da273569068ed5230f55032dd
Valeriy Popov <complexityclass@yandex-team.ru>
2019-06-09 22:36:47 +0300

    add base solution

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.12
 
 require (
 	github.com/beevik/ntp v0.2.0
-	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/net v0.0.0-20190522155817-f3200d17e092 // indirect
 )
---
+++ string_unzip/unzipper_test.go
@@ -0,0 +1,66 @@
+package string_unzip
+
+//* "a4bc2d5e" => "aaaabccddddde"
+//* "abcd" => "abcd"
+//* "45" => "" (некорректная строка)
+//* `qwe\4\5` => `qwe45` (*)
+//* `qwe\45` => `qwe44444` (*)
+//* `qwe\\5` => `qwe\\\\\` (*)
+//"Hello, 世界"
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestRegularStrings(t *testing.T) {
+	//given
+	var str1 = "a4bc2d5e"
+	var str2 = "abcd"
+	var str3 = "45"
+
+	//when
+	var res1, err1 = Unzip(str1)
+	var res2, err2 = Unzip(str2)
+	var _, err3 = Unzip(str3)
+
+	//then
+	assert.Equal(t, "aaaabccddddde", res1)
+	assert.NoError(t, err1)
+	assert.Equal(t, "abcd", res2)
+	assert.NoError(t, err2)
+	assert.Error(t, err3)
+}
+
+func TestRepeats(t *testing.T) {
+	//given
+	var str = "a4bc2d5e"
+	expected := []Pair{
+		{'a', 4},
+		{'b', 1},
+		{'c', 2},
+		{'d', 5},
+		{'e', 1}}
+
+	//when
+	var reps = repeats(str)
+
+	//then
+	assert.Equal(t, expected, reps)
+}
+
+func TestDecodeInt(t *testing.T) {
+	//given
+	var numStr1 = "14578"
+	var numStr2 = "124g"
+
+	//when
+	var res1, size1 = decodeInt([]byte(numStr1))
+	var res2, size2 = decodeInt([]byte(numStr2))
+
+	//then
+	assert.Equal(t, 14578, res1)
+	assert.Equal(t, 124, res2)
+	assert.Equal(t, 5, size1)
+	assert.Equal(t, 3, size2)
+}
